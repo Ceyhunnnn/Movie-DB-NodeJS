38cbc3c8ebf39f3b5b157aeca9fc9aceb34ac9dd
Ceyhun-gl <ceyhungul148@gmail.com>
2023-11-12 11:34:14 +0300

    mongoSanitize added

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require("express-async-errors");
 require("dotenv").config();
 require("./src/database/databaseConnection");
-
 const express = require("express");
 const port = process.env.PORT || 5001;
 const router = require("./src/routes");
@@ -9,13 +8,19 @@ const errorHandlerMiddleware = require("./src/middlewares/errorHandler");
 const cors = require("cors");
 const corsOptions = require("./src/helpers/corsOptions");
 const app = express();
+const mongoSanitize = require("express-mongo-sanitize");
+
 app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
 app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
 );
 app.use(cors(corsOptions));
-
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
 app.use("/api", router);
 app.use(errorHandlerMiddleware);
 app.listen(port, () => {
